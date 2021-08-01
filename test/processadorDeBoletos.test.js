245ba055fb397259d165dde1c24e4b5e26288b1e
João Gabriel Vilela <Sknej@users.noreply.github.com>
2021-08-01 15:34:30 -0300

    adição de segundo teste

--- test/processadorDeBoletos.test.js
+++ test/processadorDeBoletos.test.js
@@ -49,4 +49,48 @@ describe('Processador de Boletos' , () => {
             tipo: 'boleto'
         },2);
     });
+
+    test('Fatura: 1.500,00 / Boletos: 1.000,00; 500,00; 250,00 / Fatura PAGA e três pagamentos' , () => {
+        fatura.nomeCliente = 'Ana das Couves';
+        fatura.data = Date(2021,1,1);
+        fatura.valorTotal = 1500.00;
+        fatura.status = 0;
+
+        const boleto1 = {
+            codigo: '34191790010104351004791020150008485180100000',
+            data: Date(2021,2,1),
+            valor: 1000.00
+        };
+        const boleto2 = {
+            codigo: '34191790010104351004791020150008185460050000',
+            data: Date(2021,3,1),
+            valor: 500.00
+        };
+        const boleto3 = {
+            codigo: '34191790010104351004791020150008485770025000',
+            data: Date(2021,4,1),
+            valor: 250.00
+        };
+        const arr = [boleto1, boleto2, boleto3];
+
+        const pagamentos = fatura.processadorDeBoletos('boletos',arr);
+        
+        expect(fatura.status).toBe(1);
+        expect(pagamentos.length).toBe(3);
+        expect(pagamentos[0]).toBeDeepCloseTo({
+            valorPago: 1000.00,
+            data: Date(2021,2,1),
+            tipo: 'boleto'
+        },2);
+        expect(pagamentos[1]).toBeDeepCloseTo({
+            valorPago: 500.00,
+            data: Date(2021,3,1),
+            tipo: 'boleto'
+        },2);
+        expect(pagamentos[2]).toBeDeepCloseTo({
+            valorPago: 250.00,
+            data: Date(2021,4,1),
+            tipo: 'boleto'
+        },2);
+    });
 });
