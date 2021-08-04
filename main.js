a4eed867b878fda1bde37b004a71ae695aef00fc
João Gabriel Vilela <jgvilela@gmail.com>
2021-08-04 17:48:16 -0300

    primeiros exercícios da parte 2 resolvidos

--- main.js
+++ main.js
@@ -2,17 +2,25 @@ const fs = require('fs');
 const { DOMParser } = require('xmldom');
 const R = require('ramda');
 
+//Exercício 4 - as funções isValid, elementsToArray, getGitHubProject abaixo são high-order
 const { isValid, elementsToArray, getGitHubProject } = require('./xmlfilter');
 
 // efetua o processamento do xml e armazena a estrutura no objeto 'document'
+//Exercício 4 - a função parseFromString abaixo é high-order
 const document = new DOMParser().parseFromString(fs.readFileSync('res/f-droid.xml', 'utf-8'));
 
+//Exercício 1 - abaixo a declaração da função isAddedAfter2018AndUpdatedAfter2019
+//Exercício 3 - a função isAddedAfter2018AndUpdatedAfter2019 abaixo é curried
+//Exercício 4 - a função isAddedAfter2018AndUpdatedAfter2019 abaixo é high-order
 const isAddedAfter2018AndUpdatedAfter2019 = isValid(R.__, 2018, 2019);
 
+//Exercício 4 - a função filter e map abaixo são high-order
 const addedApps = elementsToArray(document.getElementsByTagName('application'))
     .filter(isAddedAfter2018AndUpdatedAfter2019)
     .map(getGitHubProject);
 
+//Exercício 2 - função join possui efeito colateral pois altera o conteúdo de addedApps
+//Exercício 4 - a função log abaixo é high-order
 console.log(addedApps.join('\n'));
  
 // (Exercício 1) Identifique todas as declarações de funções neste projeto
--- xmlfilter.js
+++ xmlfilter.js
@@ -1,15 +1,32 @@
 const R = require('ramda');
 
+//Exercício 1 - abaixo a declaração da função contentOfTag
+//Exercício 3 - a função contentOfTag abaixo é curried
 const contentOfTag = R.curry(
     (xmlNode, tagName) => xmlNode.getElementsByTagName(tagName)[0].textContent
 );
-
+//Exercício 1 - abaixo a declaração da função contentOfTag
+//Exercício 3 - a função contentOfSource abaixo é curried
+//Exercício 4 - a função contentOfSource abaixo é high-order
 const contentOfSource = contentOfTag(R.__, 'source');
+//Exercício 1 - abaixo a declaração da função contentOfAdded
+//Exercício 3 - a função contentOfAdded abaixo é curried
+//Exercício 4 - a função contentOfAdded abaixo é high-order
 const contentOfAdded = contentOfTag(R.__, 'added');
+//Exercício 1 - abaixo a declaração da função contentOfUpdated
+//Exercício 3 - a função contentOfUpdated abaixo é curried
+//Exercício 4 - a função contentOfUpdated abaixo é high-order
 const contentOfUpdated = contentOfTag(R.__, 'lastupdated');
+//Exercício 1 - abaixo a declaração da função contentOfID
+//Exercício 3 - a função contentOfID abaixo é curried
+//Exercício 4 - a função contentOfID abaixo é high-order
 const contentOfID = contentOfTag(R.__, 'id');
+//Exercício 1 - abaixo a declaração da função getGitHubProject
+//Exercício 3 - a função getGitHubProject abaixo é curried
+//Exercício 4 - a função getGitHubProject abaixo é high-order
 const getGitHubProject = xmlNode => contentOfSource(xmlNode).replace('https://github.com/', '');
 
+//Exercício 1 - abaixo a declaração da função elementsToArray
 const elementsToArray = nodes => {
     const arr = [];
     for (let i = 0; i < nodes.length; i++)
@@ -17,6 +34,8 @@ const elementsToArray = nodes => {
     return arr;
 };
 
+//Exercício 1 - abaixo a declaração da função isValid
+//Exercício 3 - a função isValid abaixo é curried
 const isValid = R.curry(
     (app, addedAfterYear, updatedAfterYear) => {
         if (!contentOfSource(app).includes('github.com'))
